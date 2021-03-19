ccf40836854abbec7f4297ade7e92ed3c688c1af
Aigul Biktasheva <meleuz13@gmail.com>
2021-03-19 14:57:21 +0500

    StatsService added

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,87 @@
+package ru.netology.stats;
+
+public class StatsService {
+
+    public int summary(int[] purchases) {
+        int sum = 0;
+        for (int purchase : purchases) {
+            sum += purchase;
+        }
+        return sum;
+    }
+
+    public int averageCost(int[] purchases) {
+        return summary(purchases) / purchases.length;
+    }
+
+    private int getLargest(int[] purchases) {
+        int largest = purchases[0];
+        for (int purchase : purchases) {
+            if (largest < purchase) {
+                largest = purchase;
+            }
+        }
+        return largest;
+    }
+
+    public int maxMonth(int[] purchases) {
+        int largest = getLargest(purchases);
+
+        int index = 0;
+        int lastLargest = 0;
+        for (int purchase : purchases) {
+            index++;
+            if (purchase == largest) {
+                lastLargest = index;
+            }
+        }
+        return lastLargest;
+    }
+
+    private int getSmallest(int[] purchases) {
+        int smallest = purchases[0];
+        for (int purchase : purchases) {
+            if (smallest > purchase) {
+                smallest = purchase;
+            }
+        }
+        return smallest;
+    }
+
+    public int minMonth(int[] purchases) {
+        int smallest = getSmallest(purchases);
+
+        int index = 0;
+        int lastSmallest = 0;
+        for (int purchase : purchases) {
+            index++;
+            if (purchase == smallest) {
+                lastSmallest = index;
+            }
+        }
+        return lastSmallest;
+    }
+
+    public int lowerMonths(int[] purchases) {
+        int averageCost = averageCost(purchases);
+        int badMonth = 0;
+        for (int purchase : purchases) {
+            if (purchase < averageCost) {
+                badMonth++;
+            }
+        }
+        return badMonth;
+    }
+
+    public int higherMonths(int[] purchases) {
+        int averageCost = averageCost(purchases);
+        int goodMonth = 0;
+        for (int purchase : purchases) {
+            if (purchase > averageCost) {
+                goodMonth++;
+            }
+        }
+        return goodMonth;
+    }
+}
+
---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,40 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatsServiceTest {
+    StatsService service = new StatsService();
+    int[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+    @Test
+    void summary() {
+        assertEquals(180, service.summary(purchases));
+    }
+
+    @Test
+    void averageCost() {
+        assertEquals(15, service.averageCost(purchases));
+    }
+
+    @Test
+    void maxMonth() {
+        assertEquals(8, service.maxMonth(purchases));
+    }
+
+    @Test
+    void minMonth() {
+        assertEquals(9, service.minMonth(purchases));
+    }
+
+    @Test
+    void lowerMonths() {
+        assertEquals(5, service.lowerMonths(purchases));
+    }
+
+    @Test
+    void higherMonths() {
+        assertEquals(5, service.higherMonths(purchases));
+    }
+}
